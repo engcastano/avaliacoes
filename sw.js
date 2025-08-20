379ca2b7dbe25a6f2684b131d1bad8a99e69dce9
engcastano <engcastano@gmail.com>
2025-08-20 16:30:43 -0300

    Avaliações
    Versão 2.0 do Sistema de Avaliações

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'avaliacoes-v1';
+const CORE_URLS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_URLS)).then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        return cached || fetch(event.request).then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+          return resp;
+        }).catch(() => caches.match('./index.html'));
+      })
+    );
+  }
+});
